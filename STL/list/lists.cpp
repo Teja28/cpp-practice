f2eae9041025e428c253e829f2e16671817d5e48
Teja28 <alurut@gmail.com>
2018-03-05 23:36:06 -0800

    Learning about lists

---
+++ STL/list/lists.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main() {
+    
+    list<int> numbers;
+
+    numbers.push_back(1);
+    numbers.push_back(2);
+    numbers.push_back(3);
+
+    numbers.push_front(0);
+    
+    list<int>::iterator it = numbers.begin();
+    it++;
+    numbers.insert(it,100);
+    cout << "Element: " << *it << endl;
+ 
+
+    list<int>::iterator eraser = numbers.begin();
+    eraser++;
+    eraser = numbers.erase(eraser);
+    cout << "Element: " << *eraser << endl;
+
+    for(list<int>::iterator it = numbers.begin(); it!= numbers.end();it++) {
+	cout << *it << endl;
+    }
+}
