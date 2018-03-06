b0b7c5575adc0cf12ff357b9422d5df304c6defb
Teja28 <alurut@gmail.com>
2018-03-06 08:43:56 -0800

    Implemented copy constructor, need to overload assignment operator for explicit deep copies

--- STL/map/personMaps.cpp
+++ STL/map/personMaps.cpp
@@ -12,6 +12,12 @@ public:
     Person(): name(), age(0) {
     }
 
+    Person(const Person &diff) {
+	cout << "Copy" << endl;
+        this->name = diff.name;
+        this->age = diff.age;
+    }
+
     Person(string name, int age):
 	name(name), age(age){
     }
@@ -25,10 +31,16 @@ public:
 
 int main(){
     map<int, Person> people;
+    
+    // uses default constructor
     people[0] = Person("Mike",40);
     people[1] = Person("Vicky",30);
     people[2] = Person("Raj",20);
-
+    
+    // uses copy constructor
+    people.insert(make_pair(43,Person("Teja",22)));
+    people.insert(make_pair(45,Person("Rob",23)));
+    
     for(map<int,Person>::iterator it = people.begin(); it != people.end();it++)
     {
 	it->second.print();
