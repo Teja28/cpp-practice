b04142f36c31566d7ae3486cd635e5e6459d2a97
Teja28 <alurut@gmail.com>
2018-03-05 17:05:50 -0800

    inspecting differences between pack() and aligned directives

--- FileHandling/binaryFiles.cpp
+++ FileHandling/binaryFiles.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 
@@ -19,7 +20,28 @@ struct Person2 {
 }__attribute__((__packed__,aligned(1)));
 
 int main() {
-    cout << sizeof(Person1) << endl;
-    cout << sizeof(Person2) << endl;
+    string fileName1 = "binaryFile1.bin";
+    string fileName2 = "binaryFile2.bin";
+    ofstream outputFile;
+    ofstream output;
+    Person1 someguy = {"Teja", 22, 200.5};
+    Person2 sameguy = {"Teja", 22, 200.5};
+    outputFile.open(fileName1, ios::binary); //interpret file as binary rather than text
+    
+    if(outputFile.is_open()) {
+        outputFile.write(reinterpret_cast<char *>(&someguy), sizeof(Person1));
+        outputFile.close();
+    }
+    else {
+	cout << "Could not read file " + fileName1 << endl;
+    }
+
+    output.open(fileName2, ios::binary);
+    if(output.is_open()){
+	output.write(reinterpret_cast<char *>(&sameguy), sizeof(Person2));
+    }
+    else {
+	cout << "Couldn't read file:" +fileName2 << endl;
+    }
 }
     
